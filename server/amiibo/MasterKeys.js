48c513a11ee1bf74e83737e74e6281782e86a1c2
Lorenzo Pilotto <lpilotto@knowmore.fr>
2019-04-18 19:06:28 +0200

    Update

--- server/amiibo/MasterKeys.js
+++ server/amiibo/MasterKeys.js
@@ -12,5 +12,28 @@ class DerivedKeys {
         this.aesIV = [];
         this.hmacKey = [];
     }
+
+    getByte(i) {
+        if (i < 16) {
+            return this.aesKey[i];
+        } else if (i < 32) {
+            return this.aesIV[i - 16];
+        } else {
+            return this.hmacKey[i - 32];
+        }
+    }
+
+    setByte(i, val) {
+        if (i < 16) {
+            this.aesKey[i] = val;
+            return;
+        } else if (i < 32) {
+            this.aesIV[i - 16] = val;
+            return;
+        } else {
+            this.hmacKey[i - 32] = val;
+            return;
+        }
+    }
 }
 exports.DerivedKeys = DerivedKeys;
--- server/amiibo/amiibo.js
+++ server/amiibo/amiibo.js
@@ -1,4 +1,5 @@
 const { MasterKeys, DerivedKeys } = require("./MasterKeys");
+const crypto = require('crypto');
 
 exports.unpack = unpack;
 
@@ -14,8 +15,10 @@ function unpack(amiiboKeys, tag) {
     let dataKeys = new DerivedKeys();
     let tagKeys = new DerivedKeys();
 
+    // Convert format
     tagToInternal(tag, internal);
 
+    // Generate keys
     amiiboKeygen(amiiboKeys.data, internal, dataKeys);
     amiiboKeygen(amiiboKeys.tag, internal, tagKeys);
 
@@ -30,6 +33,16 @@ function memcpy(destination, destinationOffset, source, sourceOffset, length) {
     for (let i = 0; i < length; i++) {
         destination[destinationOffset + i] = source[sourceOffset + i];
     }
+}
+
+function memccpy(destination, destinationOffset, source, sourceOffset, character, length) {
+    for (let i = 0; i < length; i++) {
+        destination[destinationOffset + i] = source[sourceOffset + i];
+        if (source[sourceOffset + i] == character) {
+            return destinationOffset + i + 1;
+        }
+    }
+    return null;
 }
 
 function memset(destination, destinationOffset, data, length) {
@@ -71,4 +84,78 @@ function internalToTag(internal, tag) {
 	memcpy(tag, 0x034, internal, 0x1B4, 0x020);
 	memcpy(tag, 0x000, internal, 0x1D4, 0x008);
 	memcpy(tag, 0x054, internal, 0x1DC, 0x02C);
+}
+
+function keygen(baseKey, baseSeed, derivedKeys) {
+    let preparedSeed = [];
+    let preparedSeedSize = keygenPrepareSeed(baseKey, baseSeed, preparedSeed);
+    drbgGenerateBytes(baseKey.hmacKey, preparedSeed, derivedKeys);
+}
+
+function keygenPrepareSeed(baseKey, baseSeed, output) {
+    // 1: Copy whole type string
+    let outputOffset = memccpy(output, 0, baseKey.typeString, 0, 14);
+
+    // 2: Append (16 - magicBytesSize) from the input seed
+    let leadingSeedBytes = 16 - baseKey.magicBytesSize;
+    memcpy(output, outputOffset, baseSeed, 0, leadingSeedBytes);
+    outputOffset += leadingSeedBytes;
+
+    // 3: Append all bytes from magicBytes
+    memcpy(output, outputOffset, baseKey.magicBytes, 0, baseKey.magicBytesSize);
+    outputOffset += baseKey.magicBytesSize;
+
+    // 4: Append bytes 0x10-0x1F from input seed
+    memcpy(output, outputOffset, baseSeed, 0x10, 16);
+    outputOffset += 16;
+
+    // 5: Xor last bytes 0x20-0x3F of input seed with AES XOR pad and append them
+    for (let i = 0; i < 32; i++) {
+        output[outputOffset + i] = baseSeed[i + 32] ^ baseKeys.xorPad[i];
+    }
+    outputOffset += 32;
+
+    return outputOffset;
+}
+
+/**
+ * 
+ * @param {*} hmacKey 
+ * @param {*} seed 
+ * @param {DerivedKeys} output 
+ */
+function drbgGenerateBytes(hmacKey, seed, output) {
+    const DRBG_OUTPUT_SIZE = 32;
+    let outputSize = 48;
+    let outputOffset = 0;
+    let temp = [];
+
+    let iteration = 0;
+    while (outputSize > 0) {
+        if (outputSize < DRBG_OUTPUT_SIZE) {
+            drbgStep(initHmac(hmacKey, iteration, seed), temp, 0);
+            memcpy(output, outputOffset, temp, 0, outputSize);
+            break;
+        }
+
+        drbgStep(initHmac(hmacKey, iteration, seed), output, outputOffset);
+        outputOffset += DRBG_OUTPUT_SIZE;
+        outputSize -= DRBG_OUTPUT_SIZE;
+    }
+}
+
+function initHmac(hmacKey, iteration, seed) {
+    let hmac = crypto.createHmac('sha256', new Uint8Array(hmacKey));
+    hmac.update(new Uint8Array([(iteration >> 8) & 0x0f, (iteration >> 0) & 0x0f].concat(seed)));
+    return hmac;
+}
+
+/**
+ * @param {crypto.Hmac} hmac 
+ * @param {*} output 
+ * @param {*} outputOffset 
+ */
+function drbgStep(hmac, output, outputOffset) {
+    let buf = hmac.digest('binary');
+    memcpy(output, outputOffset, buf, 0, buf.length);
 }
